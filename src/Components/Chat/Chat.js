654d5c9bc8835f38e335538e6a97dba90bf111b8
ignatiusgabrieltan <ignatiusgabrieltanwei@gmail.com>
2023-10-18 22:46:26 +0800

    ?

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,22 +1,19 @@
-import {Message} from './Message'
+import { Message } from "./Message";
 import ContextHelper from "../Helpers/ContextHelper";
-
-
 
 //<Chat chat={chat} />
 export function Chat(props) {
-    const userEmail = ContextHelper('email')
-    const messages = props.chat.map((chatMessage, index) => {
-            return <Message 
-            chatMessage={chatMessage} 
-            key={chatMessage.key} 
-            postIndex={index} 
-            sender={userEmail === chatMessage.val.user ? 'self' : 'other'} />
-    })
+  const userEmail = ContextHelper("email");
+  const messages = props.chat.map((chatMessage, index) => {
+    return (
+      <Message
+        chatMessage={chatMessage}
+        key={chatMessage.key}
+        postIndex={index}
+        sender={userEmail === chatMessage.val.user ? "self" : "other"}
+      />
+    );
+  });
 
-    return (      
-            <div className='flex flex-col max-w-screen h-full'>
-                {messages}
-            </div>
-    )
+  return <div className="max-w-screen flex h-full flex-col">{messages}</div>;
 }
